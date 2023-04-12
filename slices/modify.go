5521a66c79d88d90103b952478e86e9d58c31e68
Richard Pickering <richard.pickering@hotmail.co.uk>
2023-04-12 23:35:20 +0100

    Implemented concatenate function for slices

--- slices/modify.go
+++ slices/modify.go
@@ -1,5 +1,10 @@
 package slices
 
+// Concatenate joins two slices together, with inputA being joined with inputB following its last element.
+func Concatenate[T any](inputA, inputB []T) []T {
+	return append(inputA, inputB...)
+}
+
 // Delete removes the element at the given index from the provided input slice, returning the resulting slice.
 func Delete[T any](input []T, index int) []T {
 	inputLen := len(input)
--- slices/modify_test.go
+++ slices/modify_test.go
@@ -5,6 +5,66 @@ import (
 	"testing"
 )
 
+func TestConcatenate(t *testing.T) {
+	type args struct {
+		inputA []int
+		inputB []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "inputs are joined consecutively",
+			args: args{
+				inputA: []int{1, 2, 3},
+				inputB: []int{4, 5, 6},
+			},
+			want: []int{1, 2, 3, 4, 5, 6},
+		},
+		{
+			name: "nil first input means result is second input",
+			args: args{
+				inputA: nil,
+				inputB: []int{4, 5, 6},
+			},
+			want: []int{4, 5, 6},
+		},
+		{
+			name: "empty first input means result is second input",
+			args: args{
+				inputA: []int{},
+				inputB: []int{4, 5, 6},
+			},
+			want: []int{4, 5, 6},
+		},
+		{
+			name: "nil second input means result is first input",
+			args: args{
+				inputA: []int{1, 2, 3},
+				inputB: nil,
+			},
+			want: []int{1, 2, 3},
+		},
+		{
+			name: "empty second input means result is first input",
+			args: args{
+				inputA: []int{1, 2, 3},
+				inputB: []int{},
+			},
+			want: []int{1, 2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Concatenate(tt.args.inputA, tt.args.inputB); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Concatenate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestDelete(t *testing.T) {
 	type args struct {
 		input []int
